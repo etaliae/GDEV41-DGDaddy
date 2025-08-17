eac0b5503099689f1cf73a45c09184732b0f1cce
Almira Redoble <almira.redoble@obf.ateneo.edu>
2025-08-17 08:14:00 +0800

    yayy wrapping around

--- Homework1/Main.cpp
+++ Homework1/Main.cpp
@@ -9,16 +9,26 @@ void initialize_chars(Coordinates* g, Coordinates* p, Coordinates* e);
 void generate_grid(Coordinates* g, Coordinates* p, Coordinates* e);
 // This prints out the grid, along with the player and the enemy.
 
+void input_validation(std::string i, Coordinates* g, Coordinates* p);
+// This validates user input, and updates the player's position.
+
 int main() 
 {
     Coordinates grid, player, enemy;
+    std::string input;
 
     initialize_chars(&grid, &player, &enemy);
-    generate_grid(&grid, &player, &enemy);
 
+    do
+    {
+        generate_grid(&grid, &player, &enemy);
+        std::cin >> input;
+        input_validation(input, &grid, &player);
+    }
+    while (input != "exit");
+    
     return 0;
 }
-
 
 void initialize_chars(Coordinates* g, Coordinates* p, Coordinates* e)
 {
@@ -30,8 +40,10 @@ void initialize_chars(Coordinates* g, Coordinates* p, Coordinates* e)
     
     while (std::getline(file, temp))
     {
-        int numtemp = std::stoi(temp);
+        int numtemp;
 
+        numtemp = std::stoi(temp);
+    
         if (counter%2 == 0)
         {
             entities[counter/2]->x = numtemp;
@@ -43,6 +55,8 @@ void initialize_chars(Coordinates* g, Coordinates* p, Coordinates* e)
         
         counter++;
     };
+
+    return;
 }
 
 void generate_grid(Coordinates* g, Coordinates* p, Coordinates* e) 
@@ -97,7 +111,11 @@ void generate_grid(Coordinates* g, Coordinates* p, Coordinates* e)
 
             else if ((row == 0) || (row == rows)) // First or last row, border
             {
-                if (col == 0)
+                if (cols == 0)
+                {
+                    grid += "x---x\n";
+                }
+                else if (col == 0)
                 {
                     grid += "x---";
                 }
@@ -110,9 +128,14 @@ void generate_grid(Coordinates* g, Coordinates* p, Coordinates* e)
                     grid += "----";
                 }
             }
+
             else if (row % 2 == 0) // Border
             {
-                if (col == 0)
+                if (cols == 0)
+                {
+                    grid += "|---|\n";
+                }
+                else if (col == 0)
                 {
                     grid += "|---";
                 }
@@ -125,6 +148,7 @@ void generate_grid(Coordinates* g, Coordinates* p, Coordinates* e)
                     grid += "+---";
                 }
             }
+
             else if (row % 2 == 1) // Cell
             {
                 grid += "|   ";
@@ -135,7 +159,90 @@ void generate_grid(Coordinates* g, Coordinates* p, Coordinates* e)
             }
         }
     }
+
     std::cout << grid;
+
+    return;
+};
+
+void input_validation(std::string i, Coordinates* g, Coordinates* p)
+{
+    if (i == "exit")
+    {
+        return;
+    }
+
+    std::string valid_input[8] = {"north", "south", "east", "west",
+                                                        "n", "s", "e", "w"} ;
+
+    bool found = false;
+
+    for (int _ = 0; _ < sizeof(valid_input) / sizeof(valid_input[0]); _++ )
+    {
+        if (i == valid_input[_])
+        {
+            found = true;
+        }
+    }
+
+    int temp;
+
+    if (found)
+    {
+        if (i == "north" || i == "n" || i == "south" || i == "s")
+        {
+            if (i == "north" || i == "n")
+            {
+                temp = p->y - 1;
+            }
+            else
+            {
+                temp = p->y + 1;
+            }
+
+            if (temp == 0)
+            {
+                temp = g->y;
+            }
+
+            else if (temp == g->y + 1)
+            {
+                temp = 1;
+            }
+
+            p->y = temp;
+        }
+
+        else if (i == "east" || i == "e" || i == "west" || i == "w")
+        {
+            if (i == "east" || i == "e")
+            {
+                temp = p->x + 1;
+            }
+            else
+            {
+                temp = p->x - 1;
+            }
+
+            if (temp == 0)
+            {
+                temp = g->x;
+            }
+
+            else if (temp == g->x + 1)
+            {
+                temp = 1;
+            }
+            
+            p->x = temp;
+        }
+    }
+
+    else
+    {
+        std::cout << "Invalid input." << std::endl;
+        return;
+    }
 };
 
 /**
